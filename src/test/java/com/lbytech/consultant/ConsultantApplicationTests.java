7e42cd07e9f927b7f410715f3d9f8e15a731c388
lby-magicbook <1508201988@qq.com>
2025-10-17 18:50:17 +0800

    feat：spring boot项目初始化

---
+++ src/test/java/com/lbytech/consultant/ConsultantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lbytech.consultant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
