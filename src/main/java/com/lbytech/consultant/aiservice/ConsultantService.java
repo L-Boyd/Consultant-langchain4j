2c4891c2d867430a2815e41dff216c6233d077ed
lby-magicbook <1508201988@qq.com>
2025-10-17 20:29:14 +0800

    feat：AiServices声明式使用（默认）

--- src/main/java/com/lbytech/consultant/aiservice/ConsultantService.java
+++ src/main/java/com/lbytech/consultant/aiservice/ConsultantService.java
@@ -3,11 +3,12 @@ package com.lbytech.consultant.aiservice;
 import dev.langchain4j.service.spring.AiService;
 import dev.langchain4j.service.spring.AiServiceWiringMode;
 
-@AiService(
+/*@AiService(
         wiringMode = AiServiceWiringMode.EXPLICIT,   // AiServices创建代理对象时，手动装配
         chatModel = "openAiChatModel" // langchain4j会自动往IOC容器里注入openAiChatModel（名字默认是首字母小写）
 
-)
+)*/
+@AiService  // 默认自动装配（创建代理对象时，自动去IOC容器里找chatModel）
 public interface ConsultantService {
 
     /**
